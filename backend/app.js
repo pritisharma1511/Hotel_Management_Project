28ad4f0b4c039b40a9507d0aec7f9ebdc4c3a425
pritisharma1511 <ps0130815@gmail.com>
2025-11-02 12:20:28 +0530

    routes created

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { dbConnection } from "./database/dbConnection.js";
+import { ErrorMiddleware } from './error/error.js';
+import reservationRoute from './routes/reservationRoute.js';
 
 const app = express();
 dotenv.config({ path: './config/config.env' });
@@ -15,7 +17,11 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/v1/reservations', reservationRoute);  
 
 dbConnection();
+
+app.use(ErrorMiddleware);
+
 
 export default app;
